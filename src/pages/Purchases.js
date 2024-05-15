fc31f250f123ed64633bba9618e0a1879524b4f9
Silvia-Wachira <wachirasilvia8.com>
2024-05-15 23:02:22 +0300

    create a new component

--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import data from '../data.json'
+import Purchase from '../Components/Purchase'
+
 
 const Purchases = (props) => {
-  return (
-    <div className='flex'>
-      <span>{props.title}</span>
-      <img className='overflow-auto' src={`../assets/image/${props.img}`}/>
-      <p>{props.description}</p>
-      <p><span>From ${props.price}</span></p>    
-    </div>
-  
-  )
+
+    return (
+      <div className='grid grid-cols-3 gap-4 lg:pt-[100px] lg:px-[200px]'>{
+        data.merchandise.map((item)=> {
+        return <Purchase 
+        key={item.id}
+        img={item.coverImg}
+        title={item.title}
+        description={item.description}
+        price={item.price}
+        />
+      })}
+      </div>
+      
+
+    )
+ 
 }
 
 export default Purchases

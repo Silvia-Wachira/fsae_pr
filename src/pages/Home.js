d8bbebb3927073b27178f29df06fca996408f9c5
Silvia-Wachira <wachirasilvia8.com>
2024-05-16 23:54:38 +0300

    display the bg-image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../Components/Button";
-// import Img from "../assets/images/bball.jpg"
+import Img from "../assets/bball.jpg"
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
 			  <div className="">
 			  {/* <a href="/purchases" class="button">SHOP</a>
 			  	<a routerLink="/purchases"><Link to="purchases">SHOP</Link></a> */}
-			  	{/* <img src={Img} alt='Img' className='' /> */}
+			  	<img src={Img} alt='Img' className='' />
 				
 			  </div>
 		</div>

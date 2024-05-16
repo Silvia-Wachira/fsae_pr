8b4d8f21e9030355534925dc30bdda8bf0da1690
Silvia-Wachira <wachirasilvia8.com>
2024-05-16 23:54:13 +0300

    display the image

--- src/Components/Purchase.js
+++ src/Components/Purchase.js
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Purchase = (props) => {
   return (
-    <div className='shadow-sm py-4 px-2 text-center border border-grey-200 h-[300px]'>
-        <div>
-            <img alt='purchase-img' className='w-[40px]' src={`../assets/image/${props.img}` }/>
-            <p>{props.description}</p>
-            <p className='font-bold'>From ksh{props.price}</p>    
-            {/* <button className=' bg-purple'>Add to Cart</button> */}
-            <a className='cursor-pointer border rounded px-2 py-1 bg-gray-900 text-white' routerLink="/budget"><Link to="budget"></Link>Add to Cart</a>
+    <div className='shadow-sm py-4 px-4 text-center border border-grey-200 h-[500px]'>
+        <div className='w-full h-64'>
+          <img alt='purchase-img' className='w-full h-full object-cover' src={`../images/${props.img}` }/>
         </div>
+        <div className='mt-10 py-6 '>
+              <p className=''>{props.description}</p>
+              <p className='font-bold mt-3'>From ksh{props.price}</p>    
+              <a className='cursor-pointer border rounded  px-2 py-1 mt-4 bg-gray-900 text-white' routerLink="/budget"><Link to="budget"></Link>Add to Cart</a>
+        </div>
+
+        
     </div>
   )
 }

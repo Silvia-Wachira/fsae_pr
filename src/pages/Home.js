54abab093a7a4fc5618092d0a3c906aecc8a3703
Silvia-Wachira <wachirasilvia8.com>
2024-12-31 17:44:44 +0300

    style the home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
 import React from "react";
-// import Button from "../Components/Button";
-import Img from "../assets/bball.jpg"
-// import Img from "../assets/boxer.jpg"
-// import { Link } from "react-router-dom";
+import Img from "../assets/bball.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	   return(
-		<div className=" "> 
-			  <div className="">
-			  	<img  src={Img} alt='Img'  />
-				 <a href="/purchases" className="">
-					<button className="text-white absolute inset-0 items-center justify-center">SHOP</button>
-				</a> 
-				 
-			  </div>
-		</div>
-	   )
-}
+  return (
+    <div className="relative h-screen">
+      <img
+        src={Img}
+        alt="Basketball"
+        className="w-full h-full object-cover lg:w-screen object-cover"
+      />
+      <Link
+        to="/purchases"
+        className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl font-bold hover:bg-opacity-70 transition"
+        role="button"
+        aria-label="Go to Purchases"
+      >
+        SHOP
+      </Link>
+    </div>
+  );
+};
+
 export default Home;
